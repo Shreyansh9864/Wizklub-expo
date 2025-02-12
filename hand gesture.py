19bcb5998d88ab03ca0bc4f42dfb16f8445ef7c1
Shreyansh9864 <148971129+Shreyansh9864@users.noreply.github.com>
2025-02-12 19:32:38 +0530

    Update hand gesture.py

--- hand gesture.py
+++ hand gesture.py
@@ -46,8 +46,9 @@ while True:
     cv2.imshow("Hand Detection", frame)
 
     # Exit on pressing 'q'
-    if cv2.waitKey(1) & 0xFF == ord('q'):
+   if cv2.waitKey(1) & 0xFF == ord('q') or cv2.getWindowProperty("MediaPipe Face Detection", cv2.WND_PROP_VISIBLE) < 1:
         break
+
 
 # Release resources
 cap.release()
